31278877d5e3ff2827c94fd090caeecdeae643db
Jhon <jhon.zuluaga@uao.edu.co>
2025-08-16 11:18:20 -0500

    Funcion odd or even

--- oddOrEven.js
+++ oddOrEven.js
@@ -1,8 +1,8 @@
-function verificarParidadRegular(numero) {
-  if (numero % 2 === 0) {
-    console.log(`${numero} es par`);
+function verificarParidadRegular(x) {
+  if (x % 2 === 0) {
+    console.log(`${x} es par`);
   } else {
-    console.log(`${numero} es impar`);
+    console.log(`${x} es impar`);
   }
 }
 
@@ -10,7 +10,7 @@ verificarParidadRegular(4); // 4 es par
 verificarParidadRegular(7); // 7 es impar
 
 
-const verificarParidadFlecha = numero => console.log(numero % 2 === 0 ? `${numero} es par` : `${numero} es impar`);
+const verificarParidadFlecha = (x) => {console.log(x % 2 === 0 ? `${x} es par` : `${x} es impar`)}
 
 verificarParidadFlecha(8); // 8 es par
 verificarParidadFlecha(5); // 5 es impar
