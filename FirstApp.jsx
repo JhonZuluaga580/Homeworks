c8dd8a34342d04c4340b71c8e548f8a0e0b8636f
Jhon <jhon.zuluaga@uao.edu.co>
2025-09-03 14:59:43 -0500

    Challenge 2

---
+++ FirstApp.jsx
@@ -0,0 +1,12 @@
+const title = "FirstApp";
+
+const FirstApp = () => {
+    return (
+        <>
+            <h1> {title} </h1>
+            <span> 10 </span>
+        </>
+    )
+}
+
+export default FirstApp
